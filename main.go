1241fba4feafbbc3070a1b5181fcefc876227711
Ali-Gorgani <ali.gorgani.fj@gmail.com>
2024-07-23 17:28:30 +0330

    add gohtml file for each path

--- main.go
+++ main.go
@@ -2,57 +2,55 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filePath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	pageText := `<h1>Hello, World!</h1>
-				  <p><a href='/contact'>Contact</a></p>
-				  <p><a href='/faq'>FAQ</a></p>`
-	fmt.Fprint(w, pageText)
-}
-
-func contactFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	pageText := `<h1>Contact Page</h1>
-				 <p><a href='/'>Home</a></p>`
-	fmt.Fprint(w, pageText)
-}
-
-func faqFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	pageText := `<h1>FAQ Page</h1>
-	<ul>
-		<li><b>Q: Is there a free version?</b></li>
-		<p>A: Yes! We offer a free trial for 30 days on any paid plans.</p>
-		<li><b>Q: What are your support hours?</b></li>
-		<p>A: We have support staff answering emails 24/7, through response times may be a bit slower on weekends.</p>
-		<li><b>Q: How do i contact support?</b></li>
-		<p>A: Email us - <a href='mailto:support@lenslocked.com'> support@lenslocked.com</p>
-	</ul>
-	<p><a href='/'>Home</a></p>`
-	fmt.Fprint(w, pageText)
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactFunc(w, r)
-	case "/faq":
-		faqFunc(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
+	tpl, err := template.ParseFiles(filePath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, filePath)
+}
+
+func contactFunc(w http.ResponseWriter, r *http.Request) {
+	filePath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, filePath)
+}
+
+func faqFunc(w http.ResponseWriter, r *http.Request) {
+	filePath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, filePath)
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", pathHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactFunc)
+	r.Get("/faq", faqFunc)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Server is running on port 8080")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
