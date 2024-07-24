ae30dfa96150ae71a1892e21261caaddf78177d1
Ali-Gorgani <ali.gorgani.fj@gmail.com>
2024-07-24 10:56:04 +0330

    make templates embeded and add ParseFs to templates.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/Ali-Gorgani/lenslocked/controllers"
+	"github.com/Ali-Gorgani/lenslocked/templates"
 	"github.com/Ali-Gorgani/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -13,30 +13,16 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "home.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "contact.gohtml"))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	fmt.Println("Server is running on port 8080")
-	err = http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
--- views/template.go
+++ views/template.go
@@ -3,9 +3,25 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{htmlTpl: tpl}, nil
+}
 
 func Parse(filePath string) (Template, error) {
 	tpl, err := template.ParseFiles(filePath)
