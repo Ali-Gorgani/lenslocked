92d357f7d071ff00355d0ea1293eb2c459afc94b
Ali-Gorgani <ali.gorgani.fj@gmail.com>
2024-07-24 21:02:44 +0330

    add style and signup page

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/Ali-Gorgani/lenslocked/views"
@@ -9,5 +10,36 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "What is LensLocked?",
+			Answer:   "LensLocked is a web application for photographers to showcase and manage their portfolios.",
+		},
+		{
+			Question: "How do I create an account?",
+			Answer:   "You can create an account by clicking on the 'Sign Up' button and filling out the registration form.",
+		},
+		{
+			Question: "Is LensLocked free to use?",
+			Answer:   "Yes, LensLocked offers a free basic plan. We also have premium plans with additional features.",
+		},
+		{
+			Question: "Can I organize my photos into albums?",
+			Answer:   "Absolutely! LensLocked allows you to create and manage multiple albums to organize your photos.",
+		},
+		{
+			Question: "How can I contact support?",
+			Answer:   `You can reach our support team by emailing <a href="mailto:support@lenslocked.com">support@lenslocked.com</a> or through the <a href="/contact">Contact Us</a> form on our website.`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- main.go
+++ main.go
@@ -13,9 +13,10 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "home.gohtml"))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "contact.gohtml"))))
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "faq.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "home.gohtml", "tailwind.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.Fs, "faq.gohtml", "tailwind.gohtml"))))
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.Fs, "signup.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
